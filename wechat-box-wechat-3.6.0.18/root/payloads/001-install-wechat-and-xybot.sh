0a86a68b8e050656e41d98fcb8586127d5b159cc
Henry Yang <henryyang666@hotmail.com>
2024-03-04 20:09:06 +0800

    the method does not woek..

--- wechat-box-wechat-3.6.0.18/root/payloads/001-install-wechat-and-xybot.sh
+++ wechat-box-wechat-3.6.0.18/root/payloads/001-install-wechat-and-xybot.sh
@@ -49,7 +49,8 @@ function install() {
 function install-xybot() {
     cd ~ || exit
     echo "Start to install pip..."
-    python -m ensurepip --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple
+    wget https://bootstrap.pypa.io/get-pip.py -O get-pip.py
+    python get-pip.py -i https://pypi.tuna.tsinghua.edu.cn/simple
     echo "Complete install pip"
     echo "Start to install xybot..."
     git clone https://github.com/HenryXiaoYang/XYBot.git XYBot
