c9ccb7a6de710d788d1c9b65562b30d5f701dc73
Henry Yang <henryyang666@hotmail.com>
2024-03-07 19:58:12 +0800

    change the docker build tag to v0.0.5

--- build-wechat-service-xybot.sh
+++ build-wechat-service-xybot.sh
@@ -5,5 +5,5 @@ if [ ! -d XYBot ]; then
   git clone https://github.com/HenryXiaoYang/XYBot.git XYBot
 fi
 
-sudo docker build -t henryxiaoyang/wechat-service-xybot:latest .
+sudo docker build -t henryxiaoyang/wechat-service-xybot:v0.0.5 .
 
